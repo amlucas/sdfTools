3cc4b51a3b44b9c22623b77a9a991233751f5dc0
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-10-25 10:48:14 +0200

    more small tests

---
+++ tests/analytical/2d.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import numpy as np
+import sdfTools
+
+n = 32
+
+offs = (0., 0., 0.)
+dims = (n, n, 1)
+exts = (1.0, 1.0, 0.0)
+center = (0.5, 0.5, 0.0)
+radius = 0.4
+
+grid = sdfTools.Grid.Uniform(dims, offs, exts)
+
+sphere = sdfTools.Sdf.Sphere(center, radius, inside=True)
+
+sphere.apply(grid)
+
+grid.dumpBov("sphere2d")
---
+++ tests/analytical/plates.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import numpy as np
+import sdfTools
+
+offs = [0., 0., 0.]
+dims = [32, 32, 64]
+exts = [1.0, 2.0, 4.0]
+
+grid = sdfTools.Grid.Uniform(dims, offs, exts)
+
+sdfTools.Sdf.Plate(point  = [0.1*exts[0], 0, 0],
+                   normal = [ 1, 0, 0]).apply(grid)
+
+sdfTools.Sdf.Plate(point  = [0.9*exts[0], 0, 0],
+                   normal = [-1, 0, 0]).interiorUnion(grid)
+
+
+grid.dumpBov("plates")
