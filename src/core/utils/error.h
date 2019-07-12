85604f41b1af939cbe345fc1f4cc981f61e2813d
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-07-12 12:09:19 +0300

    add error

---
+++ src/core/utils/error.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdio>
+
+#define STRINGIZE(x) STRINGIZE2(x)
+#define STRINGIZE2(x) #x
+
+#define error(...) do {                                                 \
+    fprintf(stderr, __FILE__ ":" STRINGIZE(__LINE__) ": "               \
+            __VA_ARGS__);                                               \
+    exit(1);                                                            \
+    } while (0) 
