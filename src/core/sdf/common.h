ff3c6113467be725a37d3ab4dd8d849772b2dcb3
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-07-15 15:48:37 +0300

    wip: periodic operations

--- src/core/sdf/common.h
+++ src/core/sdf/common.h
@@ -27,10 +27,63 @@ inline void applyOperation(const SDF *sdf, Grid *grid, Op op)
             }    
 }
 
+template <typename SDF, typename Op0, typename OpP>
+void applyOperationPeriodic(const SDF *sdf, Grid *grid, Op0 opInit, OpP opPeriodic)
+{
+    auto dims = grid->getDimensions();
+    auto exts = grid->getExtents();
+    auto offs = grid->getOffsets();
+    real *data = grid->data();
+
+    real3 d = exts / dims;
+
+    for (int iz = 0; iz < dims.z; ++iz) {
+        for (int iy = 0; iy < dims.y; ++iy) {
+            for (int ix = 0; ix < dims.x; ++ix) {
+                const real3 r0 {offs.x + ix * d.x,
+                                offs.y + iy * d.y,
+                                offs.z + iz * d.z};
+
+                const int i = ix + dims.x * (iy + dims.y * iz);
+
+                auto val = opInit(sdf->at(r0), data[i]);
+                    
+                for (int dirz = -1; dirz < 2; ++dirz) {
+                    for (int diry = -1; diry < 2; ++diry) {
+                        for (int dirx = -1; dirx < 2; ++dirx) {
+                            if (dirz == 0 &&
+                                diry == 0 &&
+                                dirx == 0)
+                                continue;
+                                
+                            auto r = r0;
+                            r.x += dirx * exts.x;
+                            r.y += diry * exts.y;
+                            r.z += dirz * exts.z;
+                                
+                            val = opPeriodic(sdf->at(r), val);
+                        }
+                    }
+                }
+                data[i] = val;
+            }
+        }
+    }
+}
+
+
 template <typename SDF>
 void apply(const SDF *sdf, Grid *grid)
 {
     applyOperation(sdf, grid, [](real s, real g){return s;});
+}
+
+template <typename SDF>
+void applyPeriodic(const SDF *sdf, Grid *grid)
+{
+    applyOperationPeriodic(sdf, grid,
+                           [](real s, real g){return s;},
+                           [](real s, real g){return std::min(s, g);});
 }
 
 template <typename SDF>
