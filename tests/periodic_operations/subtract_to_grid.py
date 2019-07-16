1c21d27e39addaac2fd94bd4b5df0fb0c2d90e0d
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2019-07-16 10:33:41 +0300

    add tests for periodic operations; closes #6

---
+++ tests/periodic_operations/subtract_to_grid.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import numpy as np
+import sdfTools
+
+n = 5
+
+offs = (0., 0., 0.)
+dims = (n, n, n)
+exts = (1.0, 1.0, 1.0)
+
+center1 = (0.75, 0.5, 0.5)
+radius1 = 0.3
+center2 = (0.95, 0.5, 0.5)
+radius2 = 0.4
+
+grid = sdfTools.Grid.Uniform(dims, offs, exts)
+
+sphere1 = sdfTools.Sdf.Sphere(center1, radius1, inside=True)
+sphere2 = sdfTools.Sdf.Sphere(center2, radius2, inside=True)
+
+sphere1.applyPeriodic(grid)
+sphere2.interiorSubtractToGridPeriodic(grid)
+
+grid.dumpBov("test")
+np.savetxt("grid.txt", grid.get())
+
+# TEST: periodic_operations.subtract_to_grid
+# cd periodic_operations
+# rm -rf *.txt
+# ./subtract_to_grid.py
+# mv grid.txt grid.out.txt
+
